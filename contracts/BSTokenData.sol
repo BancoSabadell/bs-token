815b08557ad610d806911acb06c191bbedef51c6
Jose Luis Piedrahita <jlpiedrahita@gmail.com>
2017-01-18 16:22:50 +0100

    Create Account struct to store account info

--- contracts/BSTokenData.sol
+++ contracts/BSTokenData.sol
@@ -4,13 +4,21 @@ import "Ownable.sol";
 
 contract BSTokenData is Ownable {
 
+    struct Account {
+        uint256 balance;
+    }
+
     /* Total token supply */
     uint256 public totalSupply;
     /* Get the account balance */
-    mapping (address => uint256) public balanceOf;
-    
+    mapping (address => Account) internal accounts;
+
+    function balanceOf(address account) onlyOwner constant returns (uint256) {
+        return accounts[account].balance;
+    }
+
     function addToBalance(address account, uint256 amount) onlyOwner {
-        balanceOf[account] += amount;
+        accounts[account].balance += amount;
         totalSupply += amount;
     }
 
